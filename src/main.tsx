539b0f89b6a2be987bb708554fc2e05aba3d85b8
theetathao <theeta.for@mtel.co.th>
2026-07-06 13:24:39 +0700

    Use Noto Sans font

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/noto-sans/400.css";
+import "@fontsource/noto-sans/500.css";
+import "@fontsource/noto-sans/700.css";
+import "@fontsource/noto-sans/800.css";
 import { App } from "./App";
 import "./styles.css";
 
